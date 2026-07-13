c5926b51c0ca1dbd76c9460b98374629ade97dae
flohms18 <trouilletflorent@outlook.com>
2026-07-13 23:59:35 +0200

    Vue init

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+
+import Test from './components/Test.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Test></Test>
 </template>
 
 <style scoped></style>
