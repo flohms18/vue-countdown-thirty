7505b835536fa0217efaf11428b5fea90a04b647
flohms18 <trouilletflorent@outlook.com>
2026-07-14 10:35:31 +0200

    Add CSS global

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,22 +1,6 @@
 <script setup>
-
-let countodwnDate = new Date("1997-07-18");
-
-let x = setInterval(function(){
-    let now = new Date().getTime();
-    let distance = countodwnDate - now;
-
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    
-})
-
-
 </script>
 
 <template>
-    
+    <h1>This is a test</h1>
 </template>
