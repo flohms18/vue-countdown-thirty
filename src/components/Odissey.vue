60cdb6270d35bbcb01d1980eb9c8b56923b39f40
flohms18 <trouilletflorent@outlook.com>
2026-07-17 17:54:51 +0200

    Add countdown

--- src/components/Odissey.vue
+++ src/components/Odissey.vue
@@ -1,14 +1,33 @@
 <script setup>
 
-const start = ref(new Date(Date.UTC(2027, 7, 18, 3, 3, 3)))
+import { ref } from 'vue'
+
+const start = ref('Alice')
+
+let BirthdayDate = new Date("Jul 18, 2027 15:36:00").getTime();
+
+let x = setInterval(function(){
+    let NowDate = new Date().getTime();
+
+    let gap = BirthdayDate - NowDate;
+
+    var days = Math.floor(gap / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((gap % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
 
 
+})
 </script>
 
 <template>
 
-    <div class="Section_Title">
+    <div class ="Section_Title">
         <h1 class="My_Title">{{ start }}</h1>
+        <h1 id="demo"></h1>
     </div>
 
 </template>
